8182a0bd86db60c808595cb9f3f36eaa6bb2575a
AndwSX <ortiz.andw@gmail.com>
2025-03-24 22:32:13 -0500

    Modificacion layout empleado y admin -Andres

--- JS/eventos.js
+++ JS/eventos.js
@@ -1,21 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.getElementById('sidebar');
-    const mainContent = document.getElementById('mainContent');
     const sidebarToggle = document.getElementById('sidebarToggle');
 
     sidebarToggle.addEventListener('click', function () {
         // Alternar la clase "collapsed" para el sidebar
-        sidebar.classList.toggle('collapsed'); // La clase 'collapsed' oculta el sidebar
-
-        // Ajustar las clases del mainContent dependiendo de la visibilidad del sidebar
-        if (sidebar.classList.contains('collapsed')) {
-            // Si el sidebar está colapsado, hacer que el main ocupe col-12
-            mainContent.classList.remove('col-10');
-            mainContent.classList.add('col-12');
-        } else {
-            // Si el sidebar está visible, hacer que el main ocupe col-10
-            mainContent.classList.remove('col-12');
-            mainContent.classList.add('col-10');
-        }
+        sidebar.classList.toggle('collapsed');
     });
 });
